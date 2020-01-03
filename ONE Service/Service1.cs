bed97b1a8f343e2c27ee3c0568aafd12cd36897d
Luis Orostizaga <luis.orostizaga@gmail.com>
2020-01-03 10:32:24 -0300

    Add files via upload

---
+++ ONE Service/Service1.cs
@@ -0,0 +1,60 @@
+﻿using ONE_Service.Servicios;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONE_Service
+{
+    public partial class Service1 : ServiceBase
+    {
+
+        private static IScheduler sched;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void StartFromDebugger(string[] args)
+        {
+            OnStart(args);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            StdSchedulerFactory sf = new StdSchedulerFactory();
+            sched = sf.GetScheduler();
+
+            string tiempoEjecucionProcesoExtractFTP = ConfigurationManager.AppSettings["tiempoEjecucionProcesoExtractFTP"].ToString();
+
+
+            IJobDetail job2 = JobBuilder.Create<ProcesoExtractFTP>()
+              .WithIdentity("job2", "group1")
+              .Build();
+
+            ICronTrigger trigger2 = (ICronTrigger)TriggerBuilder.Create()
+                                                      .WithIdentity("trigger2", "group1")
+                                                      .WithCronSchedule(tiempoEjecucionProcesoExtractFTP)
+                                                      .Build();
+
+            DateTimeOffset ft2 = sched.ScheduleJob(job2, trigger2);
+
+
+            sched.Start();
+        }
+
+        protected override void OnStop()
+        {
+            sched.Shutdown(true);
+        }
+    }
+}
---
+++ ONE Service/Servicios/ProcesoExtractFTP.cs
@@ -0,0 +1,49 @@
+﻿using ONE_Service.Util;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONE_Service.Servicios
+{
+    public class ProcesoExtractFTP : IJob
+    {
+
+        public Semaforo semaforo = Semaforo.getInstance("ProcesoExtractFTP");
+
+        public void Execute(IJobExecutionContext context)
+        {
+
+            if (semaforo.EstaProcesando == false)
+            {
+                semaforo.EstaProcesando = true;
+
+                try
+                {
+
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                    if (semaforo.EstaProcesando == true)
+                    {
+                        Semaforo.setInstance("ProcesoExtractFTP");
+                        if (semaforo.CantidadArchivosConError > 0)
+
+                            Semaforo.setInstance("ProcesoExtractFTP");
+                    }
+                }
+            }
+
+        }
+
+    }
+}
